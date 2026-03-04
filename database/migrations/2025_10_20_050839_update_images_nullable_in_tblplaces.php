f856522168ed0f941bcf09ffabe831cc033bc2f1
Luna <itsallluna68@gmail.com>
2026-03-04 21:43:56 +0800

    removed unused reviews migration

--- database/migrations/2025_10_20_050839_update_images_nullable_in_tblplaces.php
+++ database/migrations/2025_10_20_050839_update_images_nullable_in_tblplaces.php
@@ -14,15 +14,8 @@ return new class extends Migration {
             $table->string('link1')->nullable()->change();
             $table->string('link2')->nullable()->change();
             $table->string('email')->nullable()->change();
-            $table->string('category1')->nullable()->change();
-            $table->string('category2')->nullable()->change();
-            $table->string('category3')->nullable()->change();
 
-            // Image fields
-            $table->string('image0')->nullable()->change();
-            $table->string('image1')->nullable()->change();
-            $table->string('image2')->nullable()->change();
-            $table->string('image3')->nullable()->change();
+
 
             // Default status = 1
             $table->boolean('status')->default(1)->change();
@@ -37,13 +30,6 @@ return new class extends Migration {
             $table->string('link1')->change();
             $table->string('link2')->change();
             $table->string('email')->change();
-            $table->string('category1')->change();
-            $table->string('category2')->change();
-            $table->string('category3')->change();
-            $table->string('image0')->change();
-            $table->string('image1')->change();
-            $table->string('image2')->change();
-            $table->string('image3')->change();
             $table->boolean('status')->change();
         });
     }
