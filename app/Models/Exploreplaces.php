c1a8a038a49dcdd6dec9f14fb008727ef41ac364
Luna <itsallluna68@gmail.com>
2026-03-09 22:30:47 +0800

    Fix GD extension for Intervention Image

--- app/Models/Exploreplaces.php
+++ app/Models/Exploreplaces.php
@@ -8,15 +8,16 @@ use App\Models\PlaceCategory;
 
 class Exploreplaces extends Model
 {
-    // Make sure the table name matches exactly
     use HasFactory;
+
     protected $table = 'tblplaces';
     protected $primaryKey = 'id';
     public $timestamps = false;
-    
-       protected $casts = [
+
+    protected $casts = [
         'images' => 'array',
     ];
+
     protected $fillable = [
         'name',
         'contact',
@@ -26,19 +27,14 @@ class Exploreplaces extends Model
         'link2',
         'address',
         'email',
-        'status', 'transport', 'map_link', 'opening_hours', 'is_popular', 'images'
+        'status',
+        'transport',
+        'map_link',
+        'opening_hours',
+        'is_popular',
+        'images',
+        'main_image'
     ];
-
-    // for the image
-    public function getImagesAttribute($value)
-    {
-        return $value ? json_decode($value, true) : [];
-    }
-
-    public function setImagesAttribute($value)
-    {
-        $this->attributes['images'] = $value ? json_encode($value) : null;
-    }
 
     public function categories(){
         return $this ->belongsToMany(
