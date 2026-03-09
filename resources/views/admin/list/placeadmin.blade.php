de376b822f27429752d1b96f3b152d3318ef4a1b
Luna <itsallluna68@gmail.com>
2026-03-09 18:17:42 +0800

    update places add & edit

--- resources/views/admin/list/placeadmin.blade.php
+++ resources/views/admin/list/placeadmin.blade.php
@@ -423,32 +423,9 @@
 
 
                                 <div id="view_images" class="flex flex-wrap gap-3">
-                                    {{-- @if(!empty($place->images))
-                                    @foreach($place->images as $index => $image) --}}
-                                    <div class="relative w-24 h-24">
-
-                                        {{-- IMAGE --}}
-                                        <img src="{{ asset('storage/' . $image) }}"
-                                            class="w-24 h-24 object-cover rounded-md border">
-
-                                        {{-- MAIN LABEL --}}
-                                        @if($index === 0)
-                                            <span
-                                                class="absolute bottom-0 left-0 bg-indigo-600 text-white text-xs px-2 py-1 rounded-tr-md">
-                                                Main
-                                            </span>
-                                        @endif
-
-                                        {{-- DELETE BUTTON (ALWAYS VISIBLE) --}}
-                                        <button type="button"
-                                            onclick="removeGalleryImage('{{ $place->id }}','{{ $image }}')"
-                                            class="absolute top-1 right-1 bg-red-600 hover:bg-red-700 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center shadow-md">
-                                            ✕
-                                        </button>
-
-                                    </div>
-                                    {{-- @endforeach
-                                    @endif --}}
+                                    {{-- // Images will be loaded here dynamically with JS when modal opens
+                                     --}}
+                                
                                 </div>
                                 {{-- <p>replace main image:</p>
                                 <input type="file" name="main_image" class="w-full border rounded-md px-3 py-2 mt-2">
