d1715e3ceab1b2c09e81d8eff6b6aac08d08c2e8
Luna <itsallluna68@gmail.com>
2026-03-09 17:46:28 +0800

    update places add & edit

--- resources/views/admin/list/placeadmin.blade.php
+++ resources/views/admin/list/placeadmin.blade.php
@@ -547,34 +547,27 @@
             }
 
             // Load images
-            const imageContainer = document.getElementById('view_images');
-            imageContainer.innerHTML = '';
+           const imageContainer = document.getElementById('view_images');
+imageContainer.innerHTML = '';
 
-            if (place.images) {
-                place.images.forEach((img, index) => {
-
-                    imageContainer.innerHTML += `
+if (place.images) {
+    place.images.forEach((img, index) => {
+        imageContainer.innerHTML += `
             <div class="relative w-24 h-24">
-
                 <img src="/storage/${img}" 
                      class="w-24 h-24 object-cover rounded-md border">
 
-                ${index === 0 ? `
-                    <span class="absolute bottom-0 left-0 bg-indigo-600 text-white text-xs px-2 py-1 rounded-tr-md">
-                        Main
-                    </span>
-                ` : ''}
+                ${index === 0 ? `<span class="absolute bottom-0 left-0 bg-indigo-600 text-white text-xs px-2 py-1 rounded-tr-md">Main</span>` : ''}
 
                 <button type="button"
                         onclick="removeGalleryImage(${place.id}, '${img}', this)"
                         class="absolute top-1 right-1 bg-red-600 hover:bg-red-700 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center shadow-md">
                     ✕
                 </button>
-
             </div>
         `;
-                });
-            }
+    });
+}
 
             setViewMode();
         }
