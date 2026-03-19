3cfb5a8b47b816bfb983ee973ec42f2dc64b8922
luna <itsallluna68@gmail.com>
2026-03-19 14:47:32 +0800

    Update publichome.blade.php

--- resources/views/public/publichome.blade.php
+++ resources/views/public/publichome.blade.php
@@ -24,7 +24,7 @@
   @endphp
 
   <section id="hero" class="relative h-screen flex flex-col items-center justify-center text-center text-white overflow-hidden">
-    <img src="{{ asset('image/can-carlos-city-hall.png') }}"
+    <img src="{{ asset('image/public/image/cathdral.jpg') }}"
       alt="Tourism Image"
       class="absolute inset-0 w-full h-full object-cover scale-105 animate-[float_12s_ease-in-out_infinite]" />
     <div class="absolute inset-0 bg-gradient-to-b from-black/50 to-black/70"></div>
